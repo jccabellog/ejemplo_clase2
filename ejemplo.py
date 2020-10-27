14e22db117ebc4f00fcd16c22d8ca5ecccdf67ae
jccg <=>
2020-10-27 19:39:19 -0300

    se agrega lista con funcion split

--- ejemplo.py
+++ ejemplo.py
@@ -5,6 +5,9 @@ nombre="Juan Carlos Cabello"
 nombre2="Katty Brevis"
 lista_apellidos=["Cabello","Guerrero","Brevis","Lavin"]
 direccion ="Mi casa 1234"
+nombre ="Alan Madison Turing"
+lista_nombre=nombre.split()#toma el string y lo separa dentro de una lista
+print(lista_nombre)
 
 def suma(a,b):
     resultado =a+b
