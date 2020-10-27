393d1eef8ba669feb4be3cb7b6150d0a84255910
jccg <=>
2020-10-27 19:01:26 -0300

    Se agrega funcion y nuevos comentarios

--- ejemplo.py
+++ ejemplo.py
@@ -5,3 +5,11 @@ nombre="Juan Carlos Cabello"
 nombre2="Katty Brevis"
 lista_apellidos=["Cabello","Guerrero","Brevis","Lavin"]
 direccion ="Mi casa 1234"
+
+def suma(a,b):
+    resultado =a+b
+    print("El nombre es: "+str(nombre))#para concatenar string con numeros
+    return resultado
+
+print (suma(300,200))
+ 
